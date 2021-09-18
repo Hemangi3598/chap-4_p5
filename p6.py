4c8bafb3273b74acb53357f0ad3958d58c1bf6cc
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-18 16:00:36 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,26 @@
+# python strings and operators
+# + ---> concentration
+# * ---> repetation
+# in ---> membership check
+
+s1 = " kamal "
+s2 = " classes"
+s3 = " sir"
+
+print(s1 + s2)
+print(s1 + s3)
+
+r1 = s1 + s2
+print(r1)
+r2 = s1 + s3
+print(r2)
+
+r3 = s1 * 2
+print(r3)
+r4 = s2 * 5
+print(r4)
+
+r5 = "a" in "kamal"
+print(r5)
+r6 = "z" in "kamal"
+print(r6)
